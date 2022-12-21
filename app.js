7c8e30320a86a7f98a2767d771049f4a2fb05c7b
thk61159 <tina0757023.bt07@nycu.edu.tw>
2022-12-21 14:46:10 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,40 +1,26 @@
+///////////////////////impot////////////////////////
 const express = require('express');
-const app = express();
-const port = 3000;
 const methodOverride = require('method-override');
-
-app.use(express.urlencoded({ extended: true }));
-//template engine
 const { engine } = require('express-handlebars');
-app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', './views');
-// 加入這段 code, 僅在非正式環境時, 引入 dotenv，讓 Node.js 能抓到寫在 .env 上的環境變數。
+const routes = require('./routes');
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
-//database
-const mongoose = require('mongoose'); // 載入 mongoose
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}); // 設定連線到 mongoDB connect to 環境變數
-// 取得資料庫連線狀態
-const db = mongoose.connection;
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!');
-});
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!');
-});
+require('./config/mongoose');
+
+///////////////////////setting////////////////////////
+const app = express();
+const port = 3000;
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+// 在非正式環境時, 引入 dotenv，讓 Node.js 能抓到寫在 .env 上的環境變數。
+
 app.use(methodOverride('_method'));
-///////////////////////////////////////
+///////////////////////controller////////////////////////
 
-const routes = require('./routes')
 app.use(routes);
-
 
 app.listen(3000, () => {
   console.log(`app is http://localhost:${port}`);

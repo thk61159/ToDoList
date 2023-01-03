95cca5449cec3cafde936b314e710724e41839a9
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-01-03 18:44:17 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { engine } = require('express-handlebars');
 const routes = require('./routes');
 require('./config/mongoose');
 const session = require('express-session');
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 
 ///////////////////////setting////////////////////////
 const app = express();
@@ -22,6 +24,8 @@ app.use(
     saveUninitialized: false,
   })
 );
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 ///////////////////////controller////////////////////////
 
 app.use(routes);
---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+
+//module.exports，直接匯出一個 function
+module.exports = (app) => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }),
+    (email, password, done) => {
+      User.findOne({ email })
+        .then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: 'this email has not been registered',
+            });
+          }
+          if (user.password !== password) {
+            return done(null, false, {
+              message: 'Email or Password incorrect.',
+            });
+          }
+          return done(null, user); //如果沒遇到錯誤則將user傳下去
+        })
+        .catch((err) => done(err, false));
+    }
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null));
+  });
+};
+
